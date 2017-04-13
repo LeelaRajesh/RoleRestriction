c542354a3815276963cb75ecadef7b667c62d1f9
LeelaRajesh <leelarajeshsayana@gmail.com>
2017-04-13 15:01:59 -0400

    Added view to display events list

---
+++ RoleRestriction/Controllers/EventsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RoleRestriction.Models;
+
+namespace RoleRestriction.Controllers
+{
+    public class EventsController : Controller
+    {
+        // GET: Events
+        private ApplicationDbContext _context;
+        public EventsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+        public ActionResult Index()
+        {
+            var events = _context.Events.ToList();
+            return View(events);
+        }
+    }
+}
--- RoleRestriction/Models/Event.cs
+++ RoleRestriction/Models/Event.cs
@@ -2,16 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace RoleRestriction.Models
 {
     public class Event
     {
         public int Id { get; set; }
+        [Display(Name="Event Type")]
         public string EventType { get; set; }
         public string Starring { get; set; }
         public string Date { get; set; }
         public string Venue { get; set; }
+        [Display(Name="Tickets Available")]
         public int NoOfTickets { get; set; }
     }
 }

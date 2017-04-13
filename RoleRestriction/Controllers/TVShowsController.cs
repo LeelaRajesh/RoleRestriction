090795988f47d5d55fa1b032884568d5b7aad30f
LeelaRajesh <leelarajeshsayana@gmail.com>
2017-04-12 22:48:37 -0400

    Included TV shows and a view to add new movie

---
+++ RoleRestriction/Controllers/TVShowsController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RoleRestriction.Models;
+
+namespace RoleRestriction.Controllers
+{
+    public class TVShowsController : Controller
+    {
+        // GET: TVShows
+        private ApplicationDbContext _context;
+        public TVShowsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        public ActionResult Index()
+        {
+            var TVShows = _context.TVShows.ToList();
+            return View(TVShows);
+        }
+
+        public ActionResult New()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(TVShow tvShow)
+        {
+            _context.TVShows.Add(tvShow);
+            _context.SaveChanges();
+            return RedirectToAction("Index","TVShows");
+        }
+    }
+}
